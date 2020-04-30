4158fe3ec8a7a4990218c45159aba626d33188c3
Anbia Nuansyah <53186801+SansBae@users.noreply.github.com>
2020-04-30 13:24:03 +0700

    Add files via upload

---
+++ termux.py
@@ -0,0 +1,41 @@
+import os
+from time import sleep
+
+
+a ='\033[92m'
+b ='\033[91m'
+c ='\033[0m'
+os.system('clear')
+print(a+'\t  Tombol Tambahan Termux ')
+print(a+'\t  UP,Down,right,Left, CTRL ')
+print(b+'\t  Bye:   SANSBAE')
+print('\t web : cyberinfo25.blogspot.com')
+print('\t Facebook : fb.me/anbia.nuansyah25')
+print('\t Github : https://github.com/SansBae')
+print(a+'+'*40)
+print('\nProses..')
+sleep(1)
+print(b+'\n[!] Mengambil File Default Termux')
+sleep(1)
+try:
+      os.mkdir('/data/data/com.termux/files/home/.termux')
+except:
+      pass
+print(a+'[!]Success !')
+sleep(1)
+print(b+'\n[!] menambahkan File Tambahan..')
+sleep(1)
+
+key = "extra-keys = [['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]"
+kontol = open('/data/data/com.termux/files/home/.termux/termux.properties','w')
+kontol.write(key)
+kontol.close()
+sleep(1)
+print(a+'[!] Memproses  !')
+sleep(1)
+print(b+'\n[!] Tunggu Sebentar')
+sleep(2)
+os.system('termux-reload-settings')
+print(a+'[!] Proses Selesai !! ^^'+c+'\n\nhubungi : saya lewat Web atau Fanspage Facebook*\n\n')
+os.system('xdg-open https://www.youtube.com/c/SANSBAE')
+sleep(1)
